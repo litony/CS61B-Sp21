eb78f816712ba94243f11f7d370d866f6daa823a
litony <litony>
2021-01-25 21:26:19 +0800

    complete Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,9 +2,23 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+    public static int nextNumber(int n) {
+        if (n % 2 == 0 ) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        return n;
+    }
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
+        while (n > 0) {
+            System.out.print(n + " ");
+            if (n == 1) {
+                break;
+            }
+            n = nextNumber(n);
+        }
 
     }
 }
